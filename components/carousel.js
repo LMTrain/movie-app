d322ad689521ddcd2f1099382d943b49d441e1d6
LM Train <layconmm@msn.com>
2019-12-18 20:53:11 -0600

    Create reusable components

---
+++ components/carousel.js
@@ -0,0 +1,36 @@
+
+
+
+const Carousel = () => {
+    return (
+        <div id="carouselExampleIndicators" className="carousel slide my-4" data-ride="carousel">
+            <ol className="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner" role="listbox">
+              <div className="carousel-item active">
+                <img className="d-block img-fluid" src="https://lmtrain.github.io/lm-images/assets/images/bugatti_2.jpg" alt="First slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block img-fluid" src="https://lmtrain.github.io/lm-images/assets/images/mydejavuredcar.jpg" alt="Second slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block img-fluid" src="https://lmtrain.github.io/lm-images/assets/images/futurecar9.jpg" alt="Third slide" />
+              </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="sr-only">Next</span>
+            </a>
+        </div>
+    )
+}
+
+
+export default Carousel
--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 //functional component - arrow function
-const About = () => {
-    const message = 'Hello World'
-    return (
-        <h1>Hello About Page - {message}</h1>
-    )
-}
+// WHEN TO USE:
+// 1. for smaller components
+// 2. reusable components
+// 3. presentational components, partially right, we can use HOOKS and specify state
+// const About = () => {
+//     const message = 'Hello World'
+//     return (
+//         <h1>Hello About Page - {message}</h1>
+//     )
+// }
+
+
 // const About = () => {
 //     const message = 'Hello World'
 //     return React.createElement('div', null, 'I am generating this with createElement')
 // }
+
 
 //functional component - normal or traditional function
 // function About() {
@@ -18,4 +25,20 @@ const About = () => {
 //         <h1>Hello About Page</h1>
 //     )
 // }
+
+
+//Class component
+class About extends React.Component {
+
+    render() {
+        return (
+            <h1>Hello I am class component</h1>
+        )
+    }
+}
+
+
+
+
+
 export default About
