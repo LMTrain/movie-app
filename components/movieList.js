8be7fcc856636419429789bf01710ee046782fce
LM Train <layconmm@msn.com>
2019-12-21 23:30:11 -0600

    Fix links and display descriptions in detail page

--- components/movieList.js
+++ components/movieList.js
@@ -48,12 +48,12 @@ class MovieList extends React.Component {
             (
                 <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
                     <div className="card h-100">
-                        <Link href={`/movies/${movie.id}`}> 
+                        <Link href="/movies/[id]" as={`/movies/${movie.id}`}> 
                             <a><img className="card-img-top" src={movie.image} alt="" /></a>
                         </Link>
                         <div className="card-body">
                             <h4 className="card-title">
-                                <Link href={`/movies/${movie.id}`}>
+                                <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
                                     <a>{movie.name}</a>
                                 </Link>
                             </h4>                                    
@@ -70,6 +70,7 @@ class MovieList extends React.Component {
     }
 
     render() {
+        const { movies } = this.props
         
         return (
             <>
@@ -79,7 +80,7 @@ class MovieList extends React.Component {
                     <button onClick={this.decrement}className="btn btn-primary">Decrement Number</button> */}
                     {/* <h1>{this.props.movieListCount}</h1> */}
                 {/* </div> */}
-                {this.renderMovies()}
+                {this.renderMovies(movies)}
                 
             </>
         )
