b27220c94a413e1904ef32c1aa59044b62c5dccf
LM Train <layconmm@msn.com>
2019-12-23 23:47:47 -0600

    create code to get vale from the form

--- components/movieCreateForm.js
+++ components/movieCreateForm.js
@@ -1,41 +1,79 @@
 
-
-
+import { useState } from 'react'
 
 
 
 const MovieCreateForm = () => {
 
+    const [form, setForm] = useState({
+        name: 'Some Movie',
+        description: 'Some Description'
+    })
+
     return (
         <form>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Lord of the Rings" />
+                <input
+                    value={form.name}
+                    type="text" 
+                    className="form-control" 
+                    id="name" 
+                    aria-describedby="emailHelp" 
+                    placeholder="Lord of the Rings" />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="description">Description</label>
-                <input type="text" class="form-control" id="description" placeholder="Somewhere in Middle-earth..." />
+                <input
+                    value={form.description}
+                    type="text" 
+                    className="form-control" 
+                    id="description" 
+                    placeholder="Somewhere in Middle-earth..." />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="description">Rating</label>
-                <input type="number" max="5" min="0" class="form-control" id="rating" placeholder="3" />
-                <small id="emailHelp" class="form-text text-muted">Max: 5, Min: 0 </small>
+                <input
+                    value={form.rating}
+                    type="number" 
+                    max="5" 
+                    min="0" 
+                    className="form-control" 
+                    id="rating" 
+                    placeholder="3" />
+                <small id="emailHelp" className="form-text text-muted">Max: 5, Min: 0 </small>
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="image">Image</label>
-                <input type="text" class="form-control" id="image" placeholder="http://....." />
+                <input
+                    value={form.image}
+                    type="text" 
+                    className="form-control" 
+                    id="image" 
+                    placeholder="http://....." />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="cover">Cover</label>
-                <input type="text" class="form-control" id="cover" placeholder="http://......" />
+                <input
+                    value={form.cover}
+                    type="text" 
+                    className="form-control" 
+                    id="cover" 
+                    placeholder="http://......" />
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="longDesc">Long Description</label>
-                <textarea class="form-control" id="longDesc" rows="3"></textarea>
+                <textarea
+                    value={form.longDesc}
+                    className="form-control" 
+                    id="longDesc" 
+                    rows="3"></textarea>
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="genre">Genre</label>
-                <select multiple class="form-control" id="genre">
+                <select 
+                    multiple className="form-control" 
+                    id="genre">
                 <option>drama</option>
                 <option>music</option>
                 <option>adventure</option>
